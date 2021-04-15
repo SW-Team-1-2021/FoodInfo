e0ce7bf1af7e423839062004745cee68decbcce0
Fernando Hurtado <RockFercho@gmail.com>
2021-04-15 16:58:40 -0400

    sprint1/ update server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+//const cors = require('cors');
 const db = require('./src/config/mongodb');
 
+//db.connect();
+
+//const bodyParser = require('body-parser');
+//app.use(cors());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
 const port = process.env.PORT || 8082;
+
+//require('./src/routes')(app);
 
 app.listen(port);
 console.log(`Magic happens on port ${port}`);
