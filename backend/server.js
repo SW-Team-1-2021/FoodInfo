2262303489e44629a859e33caba1ffd299a797e5
LimbergVillcaCoraite <66086678+LimbergVillcaCoraite@users.noreply.github.com>
2021-04-15 21:59:50 -0400

    structure routes and middleware
    add en routes aliments y middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const app = express();
 //const cors = require('cors');
 const db = require('./src/config/mongodb');
+
+const methodOverride = require('method-override');
 
 //db.connect();
 
@@ -16,3 +19,10 @@ const port = process.env.PORT || 8082;
 
 app.listen(port);
 console.log(`Magic happens on port ${port}`);
+
+//middleware
+
+app.use(methodOverride('_method'));
+//routes
+app.use(require('./src/routes/index'));
+app.use(require('./src/routes/aliment'));
