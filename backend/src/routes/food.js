99a8f1e8b6941301a68051d49f045b13a3c5266f
LimbergVillcaCoraite <66086678+LimbergVillcaCoraite@users.noreply.github.com>
2021-04-16 18:29:19 -0400

    validar los campos que se mandan a la bd
    se valida los campos que se manda, en aso de correctos se guardara.

--- backend/src/routes/food.js
+++ backend/src/routes/food.js
@@ -8,3 +8,54 @@ router
   .post('/', foodController.saveFood);
 
 module.exports = router;
+'use strict';
+
+const express = require('express');
+const foodController = require('../food/food.controller');
+
+const router = express.Router();
+
+router.get('food/add', (req, res) => {
+  res.render('foodtest/new-food');
+});
+
+router.post('/foodtest/new-food', (req, res) => {
+  const { categoria, nombre, segundonombre, procedencia, calegorias, horainicio, horafinal, advertencia, combinacion, descripcion, imagen } = req.body;
+  const errors = [];
+  if (!categoria) {
+    errors.push({ text: 'Por favor selecciona una categoria' });
+  }
+  if (!nombre) {
+    errors.push({ text: 'Por favor ingresa nombre' });
+  }
+  if (!procedencia) {
+    errors.push({ text: 'Por favor ingresa una procedencia' });
+  }
+  if (!calegorias) {
+    errors.push({ text: 'Por favor ingresa calorias' });
+  }
+  if (!descripcion) {
+    errors.push({ text: 'Por favor ingresa una descripcion' });
+  }
+  if (!imagen) {
+    errors.push({ text: 'Por favor ingresa una imagen' });
+  }
+  if (errors.length > 0) {
+    res.render('foodtest/new-food', {
+      errors,
+      categoria,
+      nombre,
+      procedencia,
+      calegorias,
+      descripcion,
+      imagen
+    });
+  } else {
+    router
+      .post('/', foodController.saveFood);
+    res.send('ok');
+  }
+});
+
+
+module.exports = router;
