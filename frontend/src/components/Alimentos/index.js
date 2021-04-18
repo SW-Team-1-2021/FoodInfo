548681f4660f133b65de20c760cdf2209ded933c
Yara20 <yara.ale.ok.20@gmail.com>
2021-04-18 19:48:14 -0400

    Creando tabla con encabezados

--- frontend/src/components/Alimentos/index.js
+++ frontend/src/components/Alimentos/index.js
@@ -2,12 +2,50 @@ import React from 'react';
 import './alimentos.css';
 
 function Alimentos() {
-    return(
-    <div>
-        <h1  className="titulo">LISTA DE ALIMENTOS</h1>
-    </div>
+    return (
+        <div>
+            <h1 className="titulo">LISTA DE ALIMENTOS</h1>
+            <div className="tabla">
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>Categoria</th>
+                            <th>Nombre</th>
+                            <th>Procedencia</th>
+                            <th>Calorias(kcal)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Frutas</td>
+                            <td>Manzana</td>
+                            <td>Asia</td>
+                            <td>12</td>
+                        </tr>
+                        <tr className="alt">
+                            <td>Frutas</td>
+                            <td>Manzana</td>
+                            <td>Asia</td>
+                            <td>12</td>
+                        </tr>
+                        <tr>
+                            <td>Frutas</td>
+                            <td>Manzana</td>
+                            <td>Asia</td>
+                            <td>12</td>
+                        </tr>
+                        <tr className="alt">
+                            <td>Frutas</td>
+                            <td>Manzana</td>
+                            <td>Asia</td>
+                            <td>12</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
-    
+
 }
 
 export default Alimentos;
