70add62754d844930e50ea373b33b548d7e42e66
Sebas <sebascanedomunoz@gmail.com>
2021-04-18 11:14:05 -0400

    mejoras en la ubicacion del endpoint get

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,13 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const db = require('./src/config/mongodb');
+const db = require("./src/config/mongodb");
+const port = process.env.PORT || 8080;
 
 db.connect();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.get('/alimentos', (req,res)=>{
-  res.send("<h1>tabla</h1>");
-});
+require("./src/routes/index.js")(server);
 
-const port = process.env.PORT || 8080;
-
-require('./src/routes')(server);
-
-server.listen(port);
-console.log(`Magic happens on port ${port}`);
+server.listen(port, () => console.log(`Magic happens on port ${port}`));
--- backend/src/routes/food.js
+++ backend/src/routes/food.js
@@ -4,7 +4,11 @@ const express = require('express');
 const foodController = require('../food/food.controller');
 
 const router = express.Router();
+
 router
-  .post('/', foodController.saveFood);
+    .post('/', foodController.saveFood)
+    .get('/alimentos', (req, res) => {
+        res.send('<h1>tabla de alimentos</h1>');
+    });
 
 module.exports = router;
