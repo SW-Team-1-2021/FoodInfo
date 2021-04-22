e43c403fda640dda1c9eff4b0f9e6e46184f1665
LimbergVillcaCoraite <66086678+LimbergVillcaCoraite@users.noreply.github.com>
2021-04-22 17:45:29 -0400

    Control del campo de texto Calorias
    En anteriores commit se aceptaban solo numeros enteros, se corrigio el error tal cual como indica el criterio de aceptacion, el campo aceptara 4 digitos enteros y 4 decimas como mucho

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Formulario, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError} from './elementos/Formularios';
+import React, { useState } from 'react';
+import { Formulario, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError } from './elementos/Formularios';
 
 import Input from './componentes/Input';
 import Descrip from './componentes/CampoDescripcion';
@@ -7,58 +7,58 @@ import Imagen from './componentes/CampoImagen'
 import Hora from './componentes/Horario';
 
 const App = () => {
-	const [categoria, cambiarCategoria] = useState({campo: '', valido: null});
-	const [nombre, cambiarNombre] = useState({campo: '', valido: null});
-	const [opcional, cambiarOpcional] = useState({campo: '', valido: null});
-	const [procedencia, cambiarProcedencia] = useState({campo: '', valido: null});
-	const [calorias, cambiarCalorias] = useState({campo: '', valido: null});
+	const [categoria, cambiarCategoria] = useState({ campo: '', valido: null });
+	const [nombre, cambiarNombre] = useState({ campo: '', valido: null });
+	const [opcional, cambiarOpcional] = useState({ campo: '', valido: null });
+	const [procedencia, cambiarProcedencia] = useState({ campo: '', valido: null });
+	const [calorias, cambiarCalorias] = useState({ campo: '', valido: null });
 	//const [horario, cambiarHorario] = useState({campo: '', valido: null});
 
-	const [advertencias, cambiarAdvertencias] = useState({campo: '', valido: null});
-	const [combinacion, cambiarCombinacion] = useState({campo: '', valido: null});
-    
-	const [descripcion, cambiarDescripcion] = useState({campo: '', valido: null});
+	const [advertencias, cambiarAdvertencias] = useState({ campo: '', valido: null });
+	const [combinacion, cambiarCombinacion] = useState({ campo: '', valido: null });
+
+	const [descripcion, cambiarDescripcion] = useState({ campo: '', valido: null });
 	const [formularioValido, cambiarFormularioValido] = useState(null);
 
 	const expresiones = {
-		
-		nombre1 : /^[a-zA-Z0-9_\s_ñ-]{4,80}$/, 
-        advert: /^[a-zA-Z0-9_\s_ñ,_-]{4,150}$/, 
-       combin:  /^[a-zA-Z0-9_\s_ñ_,-]{4,100}$/, 
-       descrip : /^[a-zA-Z0-9_\s_,_ñ-]{4,250}$/,
-	   calor : /^[0-9_\s_,_-]{2,8}$/,
+
+		nombre1: /^[a-zA-Z0-9_\s_ñ-]{4,80}$/,
+		advert: /^[a-zA-Z0-9_\s_ñ,_-]{4,150}$/,
+		combin: /^[a-zA-Z0-9_\s_ñ_,-]{4,100}$/,
+		descrip: /^[a-zA-Z0-9_\s_,_ñ-]{4,250}$/,
+		calor: /^[0-9_\s_,_-]{2,8}$/,
 	}
 
-	
 
-	
+
+
 
 	const onSubmit = (e) => {
 		e.preventDefault();
 
-		if(
+		if (
 			categoria.valido === 'true' &&
 			nombre.valido === 'true' &&
 			opcional.valido === 'true' &&
 			procedencia.valido === 'true' &&
 			calorias.valido === 'true' &&
-		//	horario.valido === 'true' &&
-			advertencias.valido==='true'&&
-			combinacion.valido==='true' &&
-			descripcion.valido==='true'
+			//	horario.valido === 'true' &&
+			advertencias.valido === 'true' &&
+			combinacion.valido === 'true' &&
+			descripcion.valido === 'true'
 
-			
-		){
+
+		) {
 			cambiarFormularioValido(true);
-			cambiarCategoria({campo: '', valido: ''});
-			cambiarNombre({campo: '', valido: null});
-			cambiarOpcional({campo: '', valido: null});
-			cambiarProcedencia({campo: '', valido: 'null'});
-			cambiarCalorias({campo: '', valido: null});
-		 //	cambiarHorario({campo: '', valido: null});
-			cambiarAdvertencias({campo: '', valido: null});
-			cambiarCombinacion({campo: '', valido: null});
-			cambiarDescripcion({campo: '', valido: null});
+			cambiarCategoria({ campo: '', valido: '' });
+			cambiarNombre({ campo: '', valido: null });
+			cambiarOpcional({ campo: '', valido: null });
+			cambiarProcedencia({ campo: '', valido: 'null' });
+			cambiarCalorias({ campo: '', valido: null });
+			//	cambiarHorario({campo: '', valido: null});
+			cambiarAdvertencias({ campo: '', valido: null });
+			cambiarCombinacion({ campo: '', valido: null });
+			cambiarDescripcion({ campo: '', valido: null });
 
 			// ... 
 		} else {
@@ -120,12 +120,12 @@ const App = () => {
 					leyendaError=" ingrese solo numeros "
 					expresionRegular={expresiones.calor}
 				/>
-			
+
 				<Hora
-				  mensajeError=""
+					mensajeError=""
 				/>
 
-                   <Input
+				<Input
 					estado={advertencias}
 					cambiarEstado={cambiarAdvertencias}
 					tipo="text"
@@ -146,23 +146,23 @@ const App = () => {
 					expresionRegular={expresiones.nombre1}
 				/>
 
-				 <Imagen
-				 
-				 />
+				<Imagen
+
+				/>
 
 				<Descrip
-				  estado={descripcion}
-				  cambiarEstado={cambiarDescripcion}
-				  leyendaError="La descripcion debe ser de 4 a 250 digitos sin caracteres especiales."
-			      expresionRegular={expresiones.descrip}
-				
+					estado={descripcion}
+					cambiarEstado={cambiarDescripcion}
+					leyendaError="La descripcion debe ser de 4 a 250 digitos sin caracteres especiales."
+					expresionRegular={expresiones.descrip}
+
 				/>
-                
-				
 
 
 
-				 
+
+
+
 				{formularioValido === false && <MensajeError>
 					<p>
 						<b>Error:</b> Por favor rellena el formulario correctamente.
@@ -176,5 +176,5 @@ const App = () => {
 		</main>
 	);
 }
- 
+
 export default App;
