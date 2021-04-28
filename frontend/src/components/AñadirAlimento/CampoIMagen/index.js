0ff52a6c987a997b7ebd34671585e54437f668e3
Laime1 <anlaifel09@gmail.com>
2021-04-28 18:29:56 -0400

    Imagen
    Subir otro archivo que no sea un jpg o png saldra un mensaje  que te dice que elijas una imagen

--- frontend/src/components/AñadirAlimento/CampoIMagen/index.js
+++ frontend/src/components/AñadirAlimento/CampoIMagen/index.js
@@ -1,8 +1,8 @@
 import React  from 'react';
-import {Label}  from './../estilosFormmulario';
+import {Label,LeyendaError}  from './../estilosFormmulario';
 
 
-const Imagen=({estado,cambiarEstado})=>{
+const Imagen=({estado,cambiarEstado,leyendaError})=>{
 
 
 
@@ -13,24 +13,38 @@ const Imagen=({estado,cambiarEstado})=>{
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
-                cambiarEstado({campo:e.target.result,valido:'true'})
+                cambiarEstado({campo:e.target.result,valido:true})
 
             };
             reader.readAsDataURL(input.files[0]);
         }
+
+        var archivoInput=document.getElementById('archivoInput');
+        var archivoRuta=archivoInput.value;
+        var extPermitidas=/(.PNG|.JPG)$/i;
+        if(!extPermitidas.exec(archivoRuta)){
+            alert('Asegurese de que sea una imagen');
+            archivoInput.value='';
+            
+        }
     }
+
+   
+
+    
 
     return(
         <div>
         <Label htmlFor="imagen" >Subir imagen</Label>
         
             <input 
+            id="archivoInput"
                 type="file"
-                accept="image/*"    
-                onChange={onChange}
-                required
+                accept="image/*"
+                 onChange={onChange}
+                 required
             />
-            
+        <LeyendaError valido={estado.valido}>{leyendaError} </LeyendaError>  
         
       </div>
 
