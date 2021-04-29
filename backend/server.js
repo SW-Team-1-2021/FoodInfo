8102be367022b5c2744f775944aa277060896896
sebas <sebascanedomunoz@gmail.com>
2021-04-28 22:15:07 -0400

    require path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+//const path = require('path');
 const db = require('./src/config/mongodb');
 const port = process.env.PORT || 8082;
 
