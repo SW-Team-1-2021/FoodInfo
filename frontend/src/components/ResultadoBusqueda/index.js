68c6b3deeda820ddbc7bff716c91041658cee733
Fernando Hurtado <RockFercho@gmail.com>
2021-05-10 23:43:01 -0400

    agregando la funcion para obtener datos del servidor

--- frontend/src/components/ResultadoBusqueda/index.js
+++ frontend/src/components/ResultadoBusqueda/index.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import Cards from './Cards'
 import './resultado.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function index() {
+const URL = 'http://localhost:8082/api/food';
+
+export default function Index() {
+
+    const [foods, setFoods] = useState([]);
+
+    useEffect(() => {
+        axios.get(URL,{
+            params: {
+                //name: 'PLATANO',
+                //category: 'fruta'
+            }
+        }).then((res) => {
+          setFoods(res.data);
+        });
+    }, []);
+
     return (
-        <div className="resultado">
-            <h1 align="center"><strong>RESULTADO DE BÚSQUEDA</strong></h1>
-            <Cards/>
-        </div>
+        foods.map(food => (
+            <div className="col-md-3" key={food._id}>
+                <img src={food.imagen}  alt="" width="100" height="100" />
+            </div>
+        ))
+        // <div className="resultado">
+        //     <h1 align="center"><strong>RESULTADO DE BÚSQUEDA</strong></h1>
+        //     <Cards/>
+        // </div>
     )
 }
