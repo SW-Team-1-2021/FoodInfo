7de935da71c4853f8e906c446c5bc0b644ea3475
sebas <sebascanedomunoz@gmail.com>
2021-05-11 17:38:37 -0400

    añadi las imagenes de las categorias y algunos cambios pequeños al codigo de la tabla

--- frontend/src/components/Inicio/index.js
+++ frontend/src/components/Inicio/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
 function Inicio() {
-    return (
-        <div className="container-title">
-            <h1 className="titulo-catalogo" align='center'><strong>CATÁLOGO DE ALIMENTOS</strong></h1>
-            <h3 className="subtitulo" align='center'>
-                Tipos de alimentos: descubre las
-                distintas categorias y los
-                nutrientes que aportan
-            </h3>
-        </div>
-    );
+  return (
+    <div className="container-title">
+      <h1 className="titulo-catalogo" align="center">
+        <strong>CATÁLOGO DE ALIMENTOS</strong>
+      </h1>
+      <h3 className="subtitulo" align="center">
+        Tipos de alimentos: descubre las distintas categorias y los nutrientes
+        que aportan
+      </h3>
+    </div>
+  );
 }
 
 export default Inicio;
