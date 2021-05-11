4aa4739019f9599b45a32032fd6b4a32093dbec2
Laime1 <anlaifel09@gmail.com>
2021-05-10 20:09:47 -0400

    Campos completos
    Los campos categoria, procedencia, calorias, horario de consumo, advertencias, combinacion de alimentos ya estan completos al menos como un ejemplo

--- frontend/src/components/MotrarDetalle/estilos.js
+++ frontend/src/components/MotrarDetalle/estilos.js
@@ -5,14 +5,15 @@ const CajaMayor = styled.div`
     max-width: 1000px;
     margin: 20px auto;
     display: grid;
-	grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2, auto);
-	gap: 10px;
+	  grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, auto);
+	  grid-gap: 20px;
 
 `
 const Titulo = styled.div`
   height:60px;
-  grid-column: span 3;
+  grid-column-start:1;
+  grid-column-end:4;
   h1{
     text-transform: uppercase;
   }
@@ -20,7 +21,8 @@ const Titulo = styled.div`
 `
 
 const Imagen = styled.div`
-  grid-column: span 1;
+  grid-column: 1 / 2;
+  grid-row: 2 / 4;
   h4{
     text-transform: uppercase;
   }
@@ -29,18 +31,28 @@ const Imagen = styled.div`
 `
 const Detalle = styled.div`
    grid-column: span 2;
+
 `
 const Parrafos = styled.p`
-   
+  padding: 0 10px 0 10px;
    font-zise: 16px;
+   font-weight: bold;
 `
+const Otros = styled.div`
+   display:grid;
+   grid-template-columns: 1fr 1fr;
+   
+  
+`
+
 const Subtitulos = styled.div`
   font-zise: 16px;
   font-weight: 700;
+  
 `
 const Subtitleespecial = styled.p`
     line-height: 11;
     font-weight: 700;
     font-zise: 16px;
 `
-export { CajaMayor, Titulo, Imagen, Detalle, Parrafos, Subtitulos, Subtitleespecial };
+export { CajaMayor, Titulo, Imagen, Detalle, Parrafos, Subtitulos, Subtitleespecial,Otros };
--- frontend/src/components/MotrarDetalle/index.js
+++ frontend/src/components/MotrarDetalle/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { CajaMayor, Titulo, Imagen, Detalle, Parrafos, Subtitulos, Subtitleespecial } from './estilos';
-import logo from '../../images/logo.png';
+import { CajaMayor, Titulo, Imagen, Detalle, Parrafos, Subtitulos, Otros } from './estilos';
+import papaya from '../../images/papaya.jpg';
 
 const mostrarDetalle = () => {
     return (
@@ -9,38 +9,55 @@ const mostrarDetalle = () => {
                 <h1 align="center">DETALLE DEL ALIMENTO</h1>
             </Titulo>
             <Imagen>
-                <img src={logo} height="280px" width="280px" alt="" />
+                <img src={papaya} height="300px" width="300px" alt="" />
                 <br></br>
-                <h4><b>Nombre de alimento</b></h4>
+                <h4 align="center"><b>Papaya</b></h4>
 
             </Imagen>
             <Detalle>
                 <Parrafos>
-
-                    Además contiene fibra, la cual favorece el tránsito intestinal, regulando la digestión, facilita la eliminación de colesterol malo. Aporta vitamina C, tiene un 136% de la cantidad diaria recomendada, ayudando a fijar el hierro, potenciando el sistema inmune y siendo un poderoso antiroxidante
-            </Parrafos>
-                <Subtitulos>
-
-                    CATEGORIA
-            </Subtitulos>
-                <Subtitulos>
-                    PROCEDENCIA
-            </Subtitulos>
-                <Subtitulos>
+                La papaya es una fruta tropical que se consume por su pulpa principalmente, que suele ser de color anaranjado y de sabor dulce y jugoso. ... La papaya es un alimento rico en agua y bajo en calorías, que aporta minerales como calcio, fósforo y hierro y vitaminas A y C. Contiene papaína, un enzima que favorece la digestión
+               </Parrafos>
+               <Otros>
+               <Subtitulos>        
+                    <b>CATEGORIA</b>
+              </Subtitulos>
+              <Parrafos>
+                  Frutas
+              </Parrafos>
+              <Subtitulos>
+                   <b>PROCEDENCIA</b> 
+              </Subtitulos>
+              <Parrafos>
+                  Latinoamerica
+              </Parrafos>
+              <Subtitulos>
                     CALORIAS
-            </Subtitulos>
-                <Subtitulos>
+              </Subtitulos>
+              <Parrafos>
+                  56
+              </Parrafos>
+            <Subtitulos>
                     HORARIOS DE CONSUMO
             </Subtitulos>
+            <Parrafos>
+                15:45 AM - 20:00 PM
+            </Parrafos>
                 <Subtitulos>
                     ADVERTENCIAS
-            </Subtitulos>
-                <Subtitleespecial>
-
-
+              </Subtitulos>
+            <Parrafos>
+            El consumo excesivo de papayas puede causar un efecto laxante poderoso en los intestinos y provocar irritación gastrointestinal, hinchazón dolores y diarrea. Evita comer grandes cantidades de papaya, especialmente si está inmadura.
+            </Parrafos>
+            <Subtitulos>
                     COMBINACION DE ALIMENTOS
-                </Subtitleespecial>
+           </Subtitulos>
+           <Parrafos>
+               Papaya con limon
+           </Parrafos>
+        </Otros>
             </Detalle>
+           
         </CajaMayor>
     );
 }
