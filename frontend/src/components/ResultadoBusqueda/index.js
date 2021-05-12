d6a6c2dee692fc64c0f2694a47f0bf8e888bf309
Yara <yara.ale.ok.20@gmail.com>
2021-05-11 22:08:22 -0400

    subiendo cambios para realizar conexion con axios

--- frontend/src/components/ResultadoBusqueda/index.js
+++ frontend/src/components/ResultadoBusqueda/index.js
@@ -22,14 +22,14 @@ export default function Index() {
     }, []);
 
     return (
-        foods.map(food => (
-            <div className="col-md-3" key={food._id}>
-                <img src={food.imagen}  alt="" width="100" height="100" />
-            </div>
-        ))
-        // <div className="resultado">
-        //     <h1 align="center"><strong>RESULTADO DE BÚSQUEDA</strong></h1>
-        //     <Cards/>
-        // </div>
+        // foods.map(food => (
+        //     <div className="col-md-3" key={food._id}>
+        //         <img src={food.imagen}  alt="" width="100" height="100" />
+        //     </div>
+        // ))
+        <div className="resultado">
+            <h1 align="center"><strong>RESULTADO DE BÚSQUEDA</strong></h1>
+            <Cards/>
+        </div>
     )
 }
