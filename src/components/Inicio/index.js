2810aa083a3b3d4febeb374f74a5b7b565bdb6f0
Fernando Hurtado <RockFercho@gmail.com>
2021-05-19 18:53:56 -0400

    resolviendo bug9 sobre copia de url a otra ventana o pestana y no muestra la informacion sobre la pagina de resultados

--- src/components/Inicio/index.js
+++ src/components/Inicio/index.js
@@ -1,43 +1,28 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
 import ComponentBuscador from "../Buscador";
 import "./style.css";
 import { Categorias } from "./Categorias";
-import { URL } from '../../global/const';
 
 function Inicio() {
   let history = useHistory();
 
   const accionBuscar = (text) => {
-    const params = {
-      name: text,
-    };
+    const params = `name=${text}`;
     search(params);
   };
 
   const accionBuscarCategoria = (text) => {
-    const params = {
-      category: text,
-    };
+    const params = `category=${text}`;
     search(params);
   };
 
   const search = (params) => {
-    axios
-      .get(URL, {
-        params,
-      })
-      .then((res) => {
-        const alimentos = res.data;
-        history.push({
-          pathname: "/ui/resultados",
-          state: { alimentos },
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    history.push({
+      pathname: "/ui/resultados",
+      search: `?${params}`
+    });
+    
   };
 
   return (
