2251c4462cd4bfdb7ca127ec09fe83956539041f
Fernando Hurtado <RockFercho@gmail.com>
2021-05-29 22:39:52 -0400

    refactorizado controlador para poder guardar un administrador

--- src/administrator/administrator.controller.js
+++ src/administrator/administrator.controller.js
@@ -6,8 +6,8 @@ const errorBuilder = require('../commons/error-builder');
 
 async function saveAdministrator(req, res) {
   try {
-    
-    return res.status(200).json({});
+    const admin = await model.save(req.body);
+    return res.status(200).json(admin);
   } catch (error) {
     return res.status(error.status).json(error.body);
   }
