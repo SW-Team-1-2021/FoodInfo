71c68e5f88307bf5f11a55bc2a747a27ead9feca
Fernando Hurtado <RockFercho@gmail.com>
2021-06-03 01:15:00 -0400

    Implementacion de la funcionalidad para verificar el token

--- src/commons/middleware.js
+++ src/commons/middleware.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
-const { SALT_ROUNDS } = '../config/global';
+const errorBuilder = require('../commons/error-builder');
+const { findByToken } = require('../login/login.model');
+const { SALT_ROUNDS } = require('../config/global');
 
 const METHOD = ['PUT', 'POST'];
 const PASSWORD = 'password';
 const IGNORE = [PASSWORD, 'username', 'email', 'externalId', 'imagen', 'descripcion', 'token'];
+const NO_TOKEN = 'no token';
+const INVALID_TOKEN = 'invalid token';
 
 function findValue(array, string) {
   return array.find(value => value === string);
@@ -24,7 +28,8 @@ function upercase(res, request, next) {
 
 function trimQuery(res, request, next) {
   for (const prop in request.req.query) {
-    request.req.query[prop] = request.req.query[prop].trim();
+    request.req.query[prop] =
+    request.req.query[prop].trim();
   }
   next();
 }
@@ -42,8 +47,36 @@ function encrypt(res, request, next) {
   next();
 }
 
+async function decoded(res, request, next) {
+  try {
+    const token = request.req.query.token;
+    if (token) {
+      const found = await findByToken(token);
+      if (found.length !== 1) {
+        throw errorBuilder.build(
+          INVALID_TOKEN,
+          {
+            name: 'unauthorized',
+            message: `Invalid token - The token not found in the system.`
+          });
+      }
+    } else {
+      throw errorBuilder.build(
+        NO_TOKEN,
+        {
+          name: 'unauthorized',
+          message: `No token - You need send a valid token`
+        });
+    }
+    next();
+  } catch (error) {
+    return res.res.status(error.status).json(error.body);
+  }
+}
+
 module.exports = {
   upercase,
   encrypt,
-  trimQuery
+  trimQuery,
+  decoded
 };
