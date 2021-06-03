780747f6fc56f85db0640897d5ec42a0a1ca7e56
Fernando Hurtado <RockFercho@gmail.com>
2021-06-03 01:17:54 -0400

    Implementacion del validador del token en las rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,10 +9,10 @@ const middleware = require('../commons/middleware');
 function routes(app) {
   app.use(middleware.upercase);
   app.use(middleware.trimQuery);
-  app.use('/api/food', food);
+  app.use('/api/food', middleware.decoded, food);
   app.use('/api/login', login);
   app.use('/api/logout', logout);
-  app.use('/api/administrator', administrator);
+  app.use('/api/administrator', middleware.decoded, administrator);
 }
 
 module.exports = routes;
