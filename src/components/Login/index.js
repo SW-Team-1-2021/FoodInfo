3728771d0c06fcc543ba5ead4fb321e89a4d2940
Yara <yara.ale.ok.20@gmail.com>
2021-06-15 00:09:55 -0400

    dando estilos al formulario de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,8 +5,8 @@ export default function Index() {
     return (
         <div className="contenedorPrincipal ">
             <div className="contenedorSecundario">
-                <h2>Iniciar Sesion</h2>
-                <label>Usuario</label>
+                <h2 className="titulo">Iniciar Sesión</h2>
+                <label>Correo electrónico</label>
                 <input type="text" className="form-control"></input><br></br>
                 <label>Contraseña</label>
                 <input type="password" className="form-control"></input><br></br>
