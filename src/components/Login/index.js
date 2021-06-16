c624487c41262bf7e5dab7ce5497307e22eccd91
giovanni-hue <201308464@est.umss.edu>
2021-06-15 22:26:01 -0400

    implementacion de imagen de logo al login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './login.css'
+import logo from '../../images/logo.png'
+
 
 export default function Index() {
     return (
+        
         <div className="contenedorPrincipal ">
             <div className="contenedorSecundario">
+                <img src={logo} className='image1' alt='logo' width="100%" height="100" />
                 <h2 className="titulo">Iniciar Sesión</h2>
                 <label>Correo electrónico</label>
                 <input type="text" className="form-control"></input><br></br>
@@ -13,6 +17,7 @@ export default function Index() {
                 <button className="btn btn-success">Ingresar</button>
             </div>
         </div>
+        
 
     )
 }
