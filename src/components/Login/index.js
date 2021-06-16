d165c806b1bfdf9f7bd426c9378c9c3c9e0ff59f
Yara <yara.ale.ok.20@gmail.com>
2021-06-15 23:17:00 -0400

    Arreglando posicion del logo

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,10 +8,10 @@ export default function Index() {
         
         <div className="contenedorPrincipal ">
             <div className="contenedorSecundario">
-                <img src={logo} className='image1' alt='logo' width="100%" height="100" />
                 <h2 className="titulo">Iniciar Sesión</h2>
+                <img src={logo} className='image1' alt='logo' width="100px" height="100px" />
                 <label>Correo electrónico</label>
-                <input type="text" className="form-control"></input><br></br>
+                <input type="text" className="form-control"></input>
                 <label>Contraseña</label>
                 <input type="password" className="form-control"></input><br></br>
                 <button className="btn btn-success">Ingresar</button>
