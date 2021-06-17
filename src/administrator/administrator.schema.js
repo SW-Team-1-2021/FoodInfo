08307bf7285dd840c4522cedaecff45a63af18f2
LimbergVillcaCoraite <limbervillcacoraite@gmail.com>
2021-06-17 17:42:36 -0400

    cambios en el schema segun los criterios de aceptacion
    tal como se especifican en los criterios de aceptacion es especificaron los campos: name, lastname, ci, email, datebirth y gender

--- src/administrator/administrator.schema.js
+++ src/administrator/administrator.schema.js
@@ -5,8 +5,12 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let administratorSchema = new Schema({
+  name: String,
+  lastname: String,
+  ci: String,
   email: String,
-  ci: String
+  datebirth: String,
+  gender: String
 }, {
   versionKey: false // You should be aware of the outcome after set to false
 });
