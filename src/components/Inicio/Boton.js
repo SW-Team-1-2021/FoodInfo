0f153b90a280488613f59d0b685ae8e29a0743c9
sebas <sebascanedomunoz@gmail.com>
2021-06-18 22:28:14 -0400

    añadi el elemento <button> a las categorias

--- src/components/Inicio/Boton.js
+++ src/components/Inicio/Boton.js
@@ -2,11 +2,13 @@ import "./style.css";
 
 export const Boton = (props) => {
   return (
-    <div onClick={() => props.redirigir(props.alt)} className="boton">
-      <div className='borde'>
-        <img className="imgCat" src={props.img} alt={props.alt} />
+    <button style = {{backgroundColor: '#00000000', borderColor: '#00000000'}}>
+      <div onClick={() => props.redirigir(props.alt)} className="boton">
+        <div className="borde">
+          <img className="imgCat" src={props.img} alt={props.alt} />
+        </div>
+        <div className="label">{props.alt}</div>
       </div>
-      <div className="label">{props.alt}</div>
-    </div>
+    </button>
   );
 };
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,23 +1,26 @@
 import React from "react";
-import './login.css'
-import logo from '../../images/logo.png'
-
+import "./login.css";
+import logo from "../../images/logo.png";
 
 export default function Index() {
-    return (
-        
-        <div className="contenedorPrincipal ">
-            <div className="contenedorSecundario">
-                <h2 className="titulo">Iniciar Sesión</h2>
-                <img src={logo} className='image1' alt='logo' width="100px" height="100px" />
-                <label>Correo electrónico</label>
-                <input type="text" className="form-control"></input>
-                <label>Contraseña</label>
-                <input type="password" className="form-control"></input><br></br>
-                <button className="btn btn-success">Ingresar</button>
-            </div>
-        </div>
-        
-
-    )
+  return (
+    <div className="contenedorPrincipal ">
+      <div className="contenedorSecundario">
+        <h2 className="titulo">Iniciar Sesión</h2>
+        <img
+          src={logo}
+          className="image1"
+          alt="logo"
+          width="100px"
+          height="100px"
+        />
+        <label>Correo electrónico</label>
+        <input type="text" className="form-control"></input>
+        <label>Contraseña</label>
+        <input type="password" className="form-control"></input>
+        <br></br>
+        <button className="btn btn-success">Ingresar</button>
+      </div>
+    </div>
+  );
 }
