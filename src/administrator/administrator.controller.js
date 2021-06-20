67a45f634e28e58adaf4a7ce6fd76cc2c7d8235b
Laime1 <anlaifel09@gmail.com>
2021-06-20 11:22:49 -0400

    Guardar administrator

--- src/administrator/administrator.controller.js
+++ src/administrator/administrator.controller.js
@@ -6,7 +6,18 @@ const errorBuilder = require('../commons/error-builder');
 
 async function saveAdministrator(req, res) {
   try {
-    const admin = await model.save(req.body);
+    let adminSave = req.body;
+    const result = await model.findByName(foodSave.nombre);
+    
+    if (result.length > 0) {
+      throw errorBuilder.build(
+        CONFLICT,
+        {
+          name: 'Data Repetition',
+          message: `The ${adminSave.nombres} is already saved in the DB`
+        });
+    }
+    const admin = await model.save(adminSave);
     return res.status(200).json(admin);
   } catch (error) {
     return res.status(error.status).json(error.body);
